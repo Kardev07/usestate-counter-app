817479a5a3aa8ec4d1cc92b9a7e4d8b69b6ea08f
Kardev07 <77888951+Kardev07@users.noreply.github.com>
2021-09-08 14:00:55 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [name, setName] = useState("");
+
+  const increment = () => {
+    setScore(score + 1);
+  };
+
+  const decrement = () => {
+    setScore(score - 1);
+  };
+
+  useEffect(() => {
+    console.log(name);
+    console.log(score);
+  }, [name, score]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h3 className="score">The score is {score}</h3>
+      <button className="button__decrement" onClick={decrement}>
+        decrement
+      </button>
+      <button className="button__increment" onClick={increment}>
+        increment
+      </button>
+      <input
+        className="input"
+        placeholder="enter your name"
+        key={name}
+        onChange={(e) => setName(e.target.value)}
+      />
     </div>
   );
 }
